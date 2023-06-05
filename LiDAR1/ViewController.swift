608923b6c1ce38e96fb2f7b4f1b4f0073af6c936
ersi012 <ersilda>
2023-06-05 10:09:15 -0400

    Initial Commit

---
+++ LiDAR1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LiDAR1
+//
+//  Created by VEMI Lab on 6/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
